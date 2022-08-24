5799e770a5af640ba039a2a28b665542e2f9b7c2
sai kiran <saikiran.c@sonata-software.com>
2022-08-24 17:18:08 +0530

    calcu

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,34 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sonata.two.Calculator;
+
 @SpringBootTest
 class DemoApplicationTests {
-
+	Calculator c1 = new Calculator();
+	@Test
+	public void addTest() {
+		
+		Assertions.assertEquals(5, c1.add(-5, 10));
+	}
+	
+	@Test
+	public void mulTest() {
+		
+		//Assertions.assertEquals(6, c1.div(2, 3));}
+		assertThrows(ArithmeticException.class,()->c1.div(30,0),"it is an error");
+	}
+		
+	@Test
+	public void divTest() {
+		
+		assertThrows(ArithmeticException.class,()->c1.div(1,0),"it is an error");
+	}
 	@Test
 	void contextLoads() {
 	}
